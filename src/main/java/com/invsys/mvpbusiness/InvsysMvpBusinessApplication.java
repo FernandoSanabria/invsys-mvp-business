4237cb5f35ffb01c98f416138836fc79c8ed9ac3
FernandoSanabria <ing.fernandosanabria@gmail.com>
2022-10-07 09:28:05 -0500

    initializing project with three main layers

---
+++ src/main/java/com/invsys/mvpbusiness/InvsysMvpBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.invsys.mvpbusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvsysMvpBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvsysMvpBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invsys/mvpbusiness/InvsysMvpBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invsys.mvpbusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvsysMvpBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
